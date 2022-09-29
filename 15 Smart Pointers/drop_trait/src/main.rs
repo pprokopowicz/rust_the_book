8be9c6a10066c974090dbab91113c520dc97cbcc
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2022-09-29 20:41:29 +0200

    Chapter 15 added

---
+++ 15 Smart Pointers/drop_trait/src/main.rs
@@ -0,0 +1,20 @@
+use std::mem::drop;
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
