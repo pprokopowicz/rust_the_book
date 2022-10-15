d9d279ac5f70ff04c8323b5a4ef9923e447f87c3
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2022-10-15 13:19:19 +0200

    17.3 Implementing an Object-Oriented Design Pattern added

---
+++ 17 Object-Oriented Programming Features of Rust/blog/src/lib.rs
@@ -0,0 +1,109 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content = self.state.as_ref().unwrap().new_content(self, text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+
+    fn new_content(&self, post: &Post, text: &str) -> String {
+        post.content.to_string()
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview { num_of_approves: 0 })
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn new_content(&self, post: &Post, text: &str) -> String {
+        format!("{}{}", post.content, text)
+    }
+}
+
+struct PendingReview {
+    num_of_approves: u8
+}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        if self.num_of_approves == 1 {
+            Box::new(Published {})
+        } else {
+            Box::new(PendingReview { num_of_approves: self.num_of_approves + 1 })
+        }
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
---
+++ 17 Object-Oriented Programming Features of Rust/blog2/src/main.rs
@@ -0,0 +1,14 @@
+use blog2::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
